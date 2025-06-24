9b3c030e48f3ebdbe5b919a37467f6fdafd4b6a6
Sajan-Kumar-Sahu <saajansahu.3007@gmail.com>
2025-06-24 17:56:50 +0530

    animation fix

--- src/components/ui/loading-spinner.jsx
+++ src/components/ui/loading-spinner.jsx
@@ -2,15 +2,14 @@ import React from "react";
 
 const LoadingSpinner = () => {
   return (
-    <div
-      className="flex justify-center items-center min-h-screen"
-      style={{ backgroundColor: '#182524' }}
-    >
-      <img
-        src="/assets/transparent logo.png"
-        alt="Loading..."
-        className="w-24 h-24 animate-pop"
-      />
+    <div className="flex justify-center items-center min-h-screen bg-black dark:bg-gray-900">
+       <div className="animate-pop">
+        <img
+          src="/assets/transparent logo.png"
+          alt="Loading..."
+          className="w-24 h-24"
+        />
+      </div>
     </div>
   );
 };
